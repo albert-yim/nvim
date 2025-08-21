66411035d90355cd578f51660a1e38d556d050b5
albert <albert.yim719@gmail.com>
2025-08-21 15:23:15 +0900

    add vim-maximizer and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,6 +30,8 @@ mapKey("<leader>rn", "<cmd>Lspsaga rename<CR>")
 
 -- neogit
 mapKey("<leader>gg", "<cmd>Neogit<CR>") -- open neogit
+mapKey("<leader>do", ":DiffviewOpen<CR>") -- open Diffview
+mapKey("<leader>dc", ":DiffviewClose<CR>") -- close Diffview
 
 -- telescope
 mapKey("<leader>ff", "<cmd>Telescope find_files<CR>")
@@ -50,9 +52,9 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sm", "<cmd>MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
